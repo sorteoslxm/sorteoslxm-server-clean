29b78dfde8ddb103e740b109b1ab41a6381f00bc
Musta Music <mustamusic@MacBook-Air-de-Musta.local>
2025-11-01 12:34:40 -0300

    Usa FIREBASE_SERVICE_ACCOUNT_KEY

--- config/firebase.js
+++ config/firebase.js
@@ -1,24 +1,19 @@
 import admin from "firebase-admin";
 import dotenv from "dotenv";
-
 dotenv.config();
 
-const privateKey = process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n");
+let serviceAccount;
 
-if (!privateKey) {
-  console.error("❌ No se encontró FIREBASE_PRIVATE_KEY o está mal formateada");
+try {
+  serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY);
+  console.log("🔥 Firebase conectado correctamente");
+} catch (error) {
+  console.error("❌ No se pudo parsear FIREBASE_SERVICE_ACCOUNT_KEY", error);
 }
-
-const serviceAccount = {
-  type: "service_account",
-  project_id: process.env.FIREBASE_PROJECT_ID,
-  private_key: privateKey,
-  client_email: process.env.FIREBASE_CLIENT_EMAIL,
-};
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
 
 export const db = admin.firestore();
-console.log("🔥 Firebase conectado correctamente");
+export const auth = admin.auth();
